2c4e2fc2196f77dbd05984b06764cb00cb51aebb
Renan Reis <renan.reis@locaweb.com.br>
2022-08-06 14:04:19 -0300

    feat: add user_type

--- app/Http/Controllers/Api/v1/AuthController.php
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -18,8 +18,15 @@ class AuthController extends Controller
     {
         $email = $request->input('email');
         $password = $request->input('password');
+
         if($email === 'admin@gmail.com' && $password === '12345678') {
-            return response()->json(['token' => 'base64:0b9n0RaASRq8pFIGh1VjN7KtNxcj6wB2BYQy1rNUpnU='], 200);
+            return response()->json([
+                'user' => ['id' => 1, 'name' => 'Administrador', 'email' => 'admin@gmail.com', 'user_type' => 'wholesale'],
+                'token' => 'base64:0b9n0RaASRq8pFIGh1VjN7KtNxcj6wB2BYQy1rNUpnU='], 200);
+        } else if ($email === 'user@gmail.com' && $password === '12345678') {
+            return response()->json([
+                'user' => ['id' => 1, 'name' => 'Usuário Padrão', 'email' => 'user@gmail.com', 'user_type' => 'retail'],
+                'token' => 'base64:0b9n0RaASRq8pFIGh1VjN7KtNxcj6wB2BYQy1rNUpnU='], 200);
         }else{
             return response()->json(['message' => 'Not authorized'], 401);
         }
@@ -29,6 +36,6 @@ class AuthController extends Controller
     {
         $user = $request->all();
         $this->users[] = $user;
-        return response()->json(['user' => $this->users], 201);
+        return response()->json(['user' => $this->users, 'token' => 'base64:0b9n0RaASRq8pFIGh1VjN7KtNxcj6wB2BYQy1rNUpnU='], 201);
     }
 }
